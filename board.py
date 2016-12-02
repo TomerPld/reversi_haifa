efbdfb7f3ec14cf363777a3b409f7e2f7aeebb3d
Nadav Ben Jakov <nadav@solebitlabs.com>
2016-12-02 21:39:44 +0200

    * added some basic structural stuff

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-from enum import IntEnum
+from enum import IntEnum, Enum
 
 class PlayerOptions(IntEnum):
     Player = 0
@@ -8,6 +8,12 @@ class PlayerOptions(IntEnum):
     def get_next(current):
         return PlayerOptions((current.value + 1) % len(PlayerOptions.__members__.items()))
 
+class MatrixValues(Enum):
+    empty = 0
+    player_1 = 1
+    player_2 = 2
+    neighbor = 3
+		
 class State:
     def __init__(self):
         self.matrix = []
@@ -16,12 +22,12 @@ class State:
     def init_state(self):
         for i in range(12):
             for j in range(12):
-                if (i, j) in [(5,6), (6, 5)]:
-                    self.matrix.append(1)
-                elif (i, j) in [(5, 5), (6, 6)]:
-                    self.matrix.append(2)
+                if (i, j) in [(5, 5), (6, 6)]:
+                    self.matrix.append(MatrixValues.player_1)
+                elif (i, j) in [(5,6), (6, 5)]:
+                    self.matrix.append(MatrixValues.player_2)
                 else:
-                    self.matrix.append(0)
+                    self.matrix.append(MatrixValues.empty)
         self.turn = 0
         return self
 
@@ -33,9 +39,95 @@ class Board:
     def reset(self):
         self.state = self.init_state
 
+    def clear_neighbors(self):
+        for i in range(144):
+            if self.state.matrix[i] == MatrixValues.neighbor:
+                self.state.matrix[i] = MatrixValues.empty
+        
     def get_neighbors(self):
-        pass
+        for i in range(144):
+            # find a 'stone' of the current player
+            if self.state.matrix[i] == MatrixValues(self.state.turn + 1):
+                print('own: ', i)
+                self._get_neighbors_spots(i)
+            
 
+    def _get_neighbors_spots(self, index):
+        opponent = MatrixValues.player_1 if self.state.turn == 1 else MatrixValues.player_2
+ 
+        # search up
+        if index >= 12:
+            pointer = index - 12
+            if self.state.matrix[pointer] == opponent:
+                while pointer >= 12:
+                    if self.state.matrix[pointer] != opponent:
+                        break
+                    pointer -= 12
+                if pointer >= 12 and self.state.matrix[pointer] == MatrixValues.empty:
+                    print('found ', pointer, 'searching up')
+                    self.state.matrix[pointer] = MatrixValues.neighbor
+        
+        # search down
+        if index < 132:
+            pointer = index + 12
+            if self.state.matrix[pointer] == opponent:
+                while pointer < 132:
+                    if self.state.matrix[pointer] != opponent:
+                        break
+                    pointer += 12
+                if pointer < 132 and self.state.matrix[pointer] == MatrixValues.empty:
+                    print('found ', pointer, 'searching down')
+                    self.state.matrix[pointer] = MatrixValues.neighbor
+        
+        # search right
+        if index % 12 != 11:
+            pointer = index + 1
+            if self.state.matrix[pointer] == opponent:
+                while pointer % 12 != 11:
+                    if self.state.matrix[pointer] != opponent:
+                        break
+                    pointer += 1
+                if pointer % 12 != 11 and self.state.matrix[pointer] == MatrixValues.empty:
+                    print('found ', pointer, 'searching right')
+                    self.state.matrix[pointer] = MatrixValues.neighbor
+        
+        # search left
+        if index % 12 != 0:
+            pointer = index - 1
+            if self.state.matrix[pointer] == opponent:
+                while pointer % 12 != 0:
+                    if self.state.matrix[pointer] != opponent:
+                        break
+                    pointer -= 1
+                if pointer % 12 != 0 and self.state.matrix[pointer] == MatrixValues.empty:
+                    print('found ', pointer, 'searching left')
+                    self.state.matrix[pointer] = MatrixValues.neighbor
+
+    def turn_loop(self, ui):
+        print()
+        print('turn: ', self.state.turn)
+        print('players: ', ui.players)
+        print()
+        print()
+        players = ui.players
+        self.get_neighbors()
+        if players[self.state.turn] == PlayerOptions.Player:
+            # it's a player's turn, mark his possible moves and wait for his action
+            ui.wait_for_player = True
+            
+        elif players[self.state.turn] == PlayerOptions.MinMax:
+            # call minmax
+            pass
+            self.next_turn()
+            self.clear_neighbors()
+        elif players[self.state.turn] == PlayerOptions.MinMaxAlphaBeta:
+            # call MinMaxAlphaBeta
+            pass
+            self.next_turn()
+            self.clear_neighbors()
+
+        ui.draw_board()
+    
     def set_state(self, state):
         self.state = state
 
--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
 
-from board import PlayerOptions
+from board import PlayerOptions, MatrixValues
 
 class UI:
     def __init__(self, board):
         self.board = board
+        self.wait_for_player = False
         
+        # create the main window
         self.master = Tk(className='Othello')
         self.frame = Frame(self.master)
         self.frame.grid(row=0,column=0)
@@ -14,8 +16,9 @@ class UI:
         self.buttons = []
         for i in range(12):
             for j in range(12):
-                button = Button(self.frame, text="      ", command= lambda x=i, y=j: self.player_action(x,y))			
-                button.grid(column=i,row=j)	
+                # button = Button(self.frame, text="      ", command= lambda x=i, y=j: self.player_action(x,y))			
+                button = Button(self.frame, text='{:3d}'.format(i * 12 + j), command= lambda x=i, y=j: self.player_action(x,y))			
+                button.grid(column=j,row=i)	
                 self.buttons.append(button)	
         
         # set the options menu
@@ -26,26 +29,46 @@ class UI:
         player_1_button = Button(self.frame, text=self.players[0].name, command=lambda x=0: self.toggle(x))
         player_1_button.grid(column=12,row=1)
         self.player_buttons.append(player_1_button)
+
         label_player_2 = Label(self.frame, text="Toggle Player 2")
         label_player_2.grid(column=12,row=2)	
         player_2_button = Button(self.frame, text=self.players[1].name, command=lambda x=1: self.toggle(x))
         player_2_button.grid(column=12,row=3)	
         self.player_buttons.append(player_2_button)
         
+        self.board.turn_loop(self)
         
     def toggle(self, player_num):
+        # a callback for clicking one of the playes selection toggles 
+
+        # cycle through the possible players
         new = PlayerOptions.get_next(self.players[player_num])
         self.players[player_num] = new
         self.player_buttons[player_num].config(text=new.name)
-        
+
     def loop(self):
+        # call the framework main loop
         self.master.mainloop()
     
     def player_action(self,x,y):
-        self.buttons[12 * x + y].config(bg="red")
+        # a callback for clicking the matrix
+        if self.wait_for_player:
+            index = 12 * x + y
+            print('index', index)
+            if self.board.state.matrix[index] == MatrixValues.neighbor:
+                self.board.state.matrix[index] = MatrixValues(self.board.state.turn + 1)
+                self.wait_for_player = False
+                self.board.next_turn()
+                self.board.clear_neighbors()
+                self.draw_board()
+            # self.buttons[12 * x + y].config(bg="red")
         
-
-    def draw_board(board):
-        pass
-        
+    def draw_board(self):
+        # draw the current state of the board
+        colors = {MatrixValues.empty: 'gray', 
+	              MatrixValues.player_1: 'white',
+                  MatrixValues.player_2: 'black',
+                  MatrixValues.neighbor: 'yellow'}
+        for i in range(144):
+            self.buttons[i].config(bg=colors[self.board.state.matrix[i]])
     
